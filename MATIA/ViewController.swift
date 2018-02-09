7303e3cca0047e8fa7e2dc80fc65c247b831f6af
tungnt55 <apofis1992@gmail.com>
2018-02-09 17:42:12 +0900

    fix upload image to server and add code to test with actual device.

--- MATIA/ViewController.swift
+++ MATIA/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import AVFoundation
+import Alamofire
+
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -25,6 +27,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
     
+    // Infinitely run the main loop to wait for our request.
+    // Only necessary if you are testing in the command line.
+    //RunLoop.main.run()
+
     @IBAction func openPhotoLibraryButton(_ sender: Any) {
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
             let imagePicker = UIImagePickerController()
@@ -32,44 +38,82 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             imagePicker.sourceType = .photoLibrary;
             imagePicker.allowsEditing = true
             self.present(imagePicker, animated: true, completion: nil)
+            
         }
     }
     
-    
-    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
-        imagePicked.image = image
-        dismiss(animated:true, completion: nil)
+        imagePicked.image = image               // setting the screen
+        picker.dismiss(animated:true, completion: nil)
+        
+        //hoang Get URL
+        /*let imageUrl          = info[UIImagePickerControllerReferenceURL] as! NSURL
+        let imageName         = imageUrl.lastPathComponent
+        let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        let photoURL          = NSURL(fileURLWithPath: documentDirectory)
+        let localPath         = photoURL.appendingPathComponent(imageName!)
+        print("localPath = ",localPath!)*/
+        
+        let imageData = UIImageJPEGRepresentation(image,0.5)
+        Alamofire.upload(
+            multipartFormData: { multipartFormData in
+                multipartFormData.append(imageData!, withName: "file",fileName: "unicorn.jpg", mimeType: "image/jpg")
+                //multipartFormData.append(fileContent.data(using: String.Encoding.utf8)!, withName: "rainbow.txt", fileName: "rainbow.jpg",mimeType: "image/jpg")
+        },
+            //to: "http://192.168.137.1:8000", // this is example of actual server address
+            //to: "http://localhost:8000",
+            to: "http://10.42.0.1:8000",
+            encodingCompletion: { encodingResult in
+                switch encodingResult {
+                case .success(let upload, _, _):
+                    upload.responseJSON { response in
+                        debugPrint(response)
+                    }
+                case .failure(let encodingError):
+                    print(encodingError)
+                }
+        }
+        )
+    }
+    //hoang
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
+        dismiss(animated: true, completion: nil)
     }
     
-    
+    /*
     @IBAction func saveButt(_ sender: Any) {
         let imageData = UIImageJPEGRepresentation(imagePicked.image!, 0.6)
         let compressedJPGImage = UIImage(data: imageData!)
         UIImageWriteToSavedPhotosAlbum(compressedJPGImage!, nil, nil, nil)
         
         let alert = UIAlertController(title: "Wow",
-                                message: "Your image has been saved to Photo Library!",
-                                preferredStyle: .alert)
+                                      message: "Your image has been saved to Photo Library!",
+                                      preferredStyle: .alert)
         let action1 = UIAlertAction(title: "Default", style: .default) { (action:UIAlertAction) in
             print("You've pressed SAVE button");
         }
         alert.addAction(action1)
     }
+    */
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        myUtterance = AVSpeechUtterance(string: "Welcome to MATIA. I am an second eyes for you! Please take a picture.")
+        myUtterance = AVSpeechUtterance(string: "Welcome to MATIA. I am an second eyes for you!")
         myUtterance.rate = 0.3
         synth.speak(myUtterance)
+        //let params = ["id": 1]
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
 }
 
